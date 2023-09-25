2180d5048733b1e9585574b5e4c2f64b94443d31
MAbdiFirdaus <abdidaus12@gmail.com>
2023-09-25 22:49:07 +0700

    update

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,9 +3,13 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
+use App\Models\KelasModel;
+
 
 class UserController extends BaseController
 {
+    protected $helpers=['form'];
     public function index()
     {
         //
@@ -23,10 +27,66 @@ class UserController extends BaseController
     }
 
     public function create(){
-        return view('create_user');
+
+        $kelas = [
+            [
+                'id' => 1,
+                'nama_kelas' => 'A'
+            ],
+
+            [
+                'id' => 2,
+                'nama_kelas' => 'B'
+            ],
+
+            [
+                'id' => 3,
+                'nama_kelas' => 'C'
+            ],
+
+            [
+                'id' => 4,
+                'nama_kelas' => 'D'
+            ],
+        ];
+
+        $data = [
+            'kelas' => $kelas
+        ];
+        
+
+        return view('create_user', $data);
     }
 
     public function store(){
+        $kelasModel = new KelasModel();
+        if($this->request->getVar('kelas') != ''){
+            $kelas_select = $kelasModel->where('id', $this->request->getVar('kelas'))->first();
+            $nama_kelas = $kelas_select['nama_kelas'];
+        }else{
+            $nama_kelas = '';
+        }
+        $userModel = new UserModel();
+        // validation
+        if(!$this->validate([
+            'nama' => 'required|alpha_space|is_unique[user.nama]',
+            'npm' => 'required|is_unique[user.npm]|integer|min_length[10]',
+            'kelas' => 'required'
+        ])){
+
+            session()->setFlashdata('nama_kelas');
+            return redirect()->back()->withInput()->with('nama_kelas', $nama_kelas);
+    }
+        
+        
+        $userModel = new UserModel();
+
+        $userModel->saveUser([
+            'nama' => $this->request->getVar('nama'),
+            'id_kelas' => $this->request->getVar('kelas'),
+            'npm' => $this->request->getVar('npm'),
+        ]);
+
         $data = [
             'nama' => $this->request->getVar('nama'),
             'kelas' => $this->request->getVar('kelas'),
